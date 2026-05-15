e7041918ad9df3c43d589097645a0c55dab32822
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2026-05-15 17:58:44 +0200

    feat(protocol): support zstd backend compression

--- proxy/build.gradle.kts
+++ proxy/build.gradle.kts
@@ -105,6 +105,7 @@ dependencies {
     implementation(libs.netty.transport.native.kqueue)
     implementation(variantOf(libs.netty.transport.native.kqueue) { classifier("osx-x86_64") })
     implementation(variantOf(libs.netty.transport.native.kqueue) { classifier("osx-aarch_64") })
+    implementation(libs.zstd.jni)
 
     implementation(libs.jopt)
     implementation(libs.terminalconsoleappender)
